1ffd60eac52b1b76ad19be45b819ca72b19aeaa4
yinmsk <galaxy220111@gmail.com>
2022-06-17 16:43:54 +0900

    no message

---
+++ DRF_0617/user/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import login, authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class UserApiView(APIView):
+    # 로그인
+    def post(self, request):
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        login(request, user)
+        return Response({"message": "로그인 성공!!"}, status=status.HTTP_200_OK)
